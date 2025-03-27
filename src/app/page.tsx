0b0322237f5429ed06ba7d73ea85bd44523d9651
balazs.saros <balazs.saros@gmail.com>
2025-03-27 18:43:28 +0100

    add copy scripts + 1 release to data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ const MusicTile: React.FC<MusicTileProps> = ({ cover, artists, title, media = ''
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8 w-full p-4 sm:p-16 justify-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-8 w-full p-4 sm:p-16 justify-items-center">
         {releases.map(({ artistRymIds, title, rymId, cover, media }) => (
           <MusicTile
             key={rymId}
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,29 +15,68 @@ type Release = {
   }
 }
 
+/* dev console artist info -> copy to clipboard and paste it here
+copy(
+  JSON.stringify(
+    [...document.querySelectorAll('.album_info a.artist')].map((artist) => ({
+      displayName: artist.innerText,
+      rymId: artist.title,
+      rymUrl: artist.href,
+    })),
+    null,
+    2
+  ).replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, (match) => match.replace(/"/g, '')).slice(1, -2).concat(',')
+)
+ */
+
 export const artists: Artist[] = [
   {
-    rymId: '[Artist661586]',
     displayName: 'Action Bronson',
+    rymId: '[Artist661586]',
     rymUrl: 'https://rateyourmusic.com/artist/action-bronson',
   },
   {
-    rymId: '[Artist45522]',
     displayName: 'The Alchemist',
+    rymId: '[Artist45522]',
     rymUrl: 'https://rateyourmusic.com/artist/the-alchemist-2',
   },
   {
-    rymId: '[Artist684375]',
     displayName: 'The Shaolin Afronauts',
+    rymId: '[Artist684375]',
     rymUrl: 'https://rateyourmusic.com/artist/the-shaolin-afronauts',
   },
   {
-    rymId: '[Artist178378]',
     displayName: 'Studio',
+    rymId: '[Artist178378]',
     rymUrl: 'https://rateyourmusic.com/artist/studio',
+  },
+  {
+    displayName: 'Juaneco y Su Combo',
+    rymId: '[Artist252287]',
+    rymUrl: 'https://rateyourmusic.com/artist/juaneco_y_su_combo',
   },
 ]
 
+/* dev console release info -> copy to clipboard and paste it here
+copy(
+  JSON.stringify(
+    {
+      artistRymIds: [...document.querySelectorAll('.album_info a.artist')].map((artist) => artist.title),
+      title: document.querySelector('.album_title').innerText,
+      rymId: document.querySelector('input.album_shortcut').value,
+      cover: document.querySelector('.page_release_art_frame img').src.split('/').slice(-1)[0].split('.')[0] + '.webp',
+      media: {
+        spotify: document.querySelector('a.ui_media_link_btn_spotify').href,
+      },
+    },
+    null,
+    2
+  ).replace(/^[\t ]*"[^:\n\r]+(?<!\\)":/gm, (match) => match.replace(/"/g, '')).concat(',')
+)
+
+
+[...document.querySelectorAll('.album_info a.artist')].map((artist) => artist.title)
+ */
 export const releases: Release[] = [
   {
     artistRymIds: ['[Artist661586]', '[Artist45522]'],
@@ -64,6 +103,15 @@ export const releases: Release[] = [
     cover: 'studio-west-coast-Cover-Art.webp',
     media: {
       spotify: 'https://open.spotify.com/album/545TSuBXtKERt6xHTpQDSA',
+    },
+  },
+  {
+    artistRymIds: ['[Artist252287]'],
+    title: 'Leyenda amazónica',
+    rymId: '[Album7288107]',
+    cover: 'juaneco-y-su-combo-leyenda-amazonica-Cover-Art.webp',
+    media: {
+      spotify: 'https://open.spotify.com/album/1XBTiJCFR4XFvymIUlnkKE',
     },
   },
 ]
