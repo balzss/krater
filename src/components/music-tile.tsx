2aed1a63496224b35ccc98b71b0ca437b27b0b2f
balazs.saros <balazs.saros@gmail.com>
2025-03-28 12:26:00 +0100

    square tiles

--- src/components/music-tile.tsx
+++ src/components/music-tile.tsx
@@ -8,15 +8,17 @@ interface MusicTileProps {
 const MusicTile: React.FC<MusicTileProps> = ({ cover, artists, title, media = '' }) => {
   return (
     <a
-      className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl duration-300 max-w-sm w-full h-auto transform transition hover:scale-103 cursor-pointer ease-in-out"
+      className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl duration-300 max-w-sm w-full transform transition hover:scale-103 cursor-pointer ease-in-out"
       href={media}
       target="_blank"
     >
-      <img
-        src={'/krater/covers/' + cover}
-        alt={title}
-        className="w-full max-h-sm h-auto object-cover"
-      />
+      <div className="relative w-full aspect-square">
+        <img
+          src={'/krater/covers/' + cover}
+          alt={title}
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+      </div>
       <div className="p-4">
         <h3 className="text-lg font-bold text-gray-900">{title}</h3>
         <p className="text-sm text-gray-600">{artists.join(', ')}</p>
