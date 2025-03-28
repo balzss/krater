7c49d32c7ec29bbae4a5e9005b6ae2da6abe803e
balazs.saros <balazs.saros@gmail.com>
2025-03-28 16:16:05 +0100

    main menu

---
+++ src/app/browse/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { releases, artists, Release } from '@/lib/data'
+import { MusicTile, SearchInput } from '@/components'
+
+export default function BrowsePage() {
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [releaseResults, setReleaseResults] = useState<Release[]>([])
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setReleaseResults(releases.sort(() => Math.random() - 0.5))
+      return
+    }
+    const lowerSearchQuery = searchQuery.toLowerCase()
+    const filteredResults = releases.filter((r) => {
+      const releaseArtists = r.artistRymIds
+        .map((id) => artists.find((artist) => artist.rymId === id)?.displayName.toLowerCase())
+        .join(' ')
+      return (
+        releaseArtists.includes(lowerSearchQuery) ||
+        r.title.toLowerCase().includes(lowerSearchQuery)
+      )
+    })
+    setReleaseResults(filteredResults)
+  }, [searchQuery])
+
+  return (
+    <div className="flex items-center flex-col m-4 sm:my-12 sm:mx-16">
+      <SearchInput
+        query={searchQuery}
+        onQueryChange={(newSearchQuery) => setSearchQuery(newSearchQuery)}
+      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-8 w-full justify-items-center m-4 sm:m-12">
+        {releaseResults.map(({ artistRymIds, title, rymId, cover, media }) => (
+          <MusicTile
+            key={rymId}
+            cover={cover}
+            artists={artistRymIds.map(
+              (id) => artists.find((artist) => artist.rymId === id)?.displayName as string
+            )}
+            title={title}
+            media={media.spotify}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,31 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import { releases, artists, Release } from '@/lib/data'
-import { MusicTile, SearchInput } from '@/components'
+import { Library, User, Dices } from 'lucide-react'
+import { releases, artists } from '@/lib/data'
 
 export default function Home() {
-  const [searchQuery, setSearchQuery] = useState<string>('')
-  const [releaseResults, setReleaseResults] = useState<Release[]>([])
-
-  useEffect(() => {
-    if (!searchQuery) {
-      setReleaseResults(releases.sort(() => Math.random() - 0.5))
-      return
-    }
-    const lowerSearchQuery = searchQuery.toLowerCase()
-    const filteredResults = releases.filter((r) => {
-      const releaseArtists = r.artistRymIds
-        .map((id) => artists.find((artist) => artist.rymId === id)?.displayName.toLowerCase())
-        .join(' ')
-      return (
-        releaseArtists.includes(lowerSearchQuery) ||
-        r.title.toLowerCase().includes(lowerSearchQuery)
-      )
-    })
-    setReleaseResults(filteredResults)
-  }, [searchQuery])
-
   return (
-    <div className="flex items-center flex-col m-4 sm:my-12 sm:mx-16">
-      <SearchInput
-        query={searchQuery}
-        onQueryChange={(newSearchQuery) => setSearchQuery(newSearchQuery)}
-      />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-8 w-full justify-items-center m-4 sm:m-12">
-        {releaseResults.map(({ artistRymIds, title, rymId, cover, media }) => (
-          <MusicTile
-            key={rymId}
-            cover={cover}
-            artists={artistRymIds.map(
-              (id) => artists.find((artist) => artist.rymId === id)?.displayName as string
-            )}
-            title={title}
-            media={media.spotify}
-          />
-        ))}
-      </div>
+    <div className="flex items-center flex-col m-4 sm:my-12 sm:mx-16 gap-4 sm:gap-8">
+      <a
+        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent max-w-sm cursor-pointer duration-300 transform transition hover:scale-103 ease-in-out flex gap-2 items-center"
+        href="/krater/browse"
+      >
+        <Library size={20} />
+        Browse all {releases.length} releases
+      </a>
+
+      <a className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent max-w-sm cursor-pointer duration-300 transform transition hover:scale-103 ease-in-out flex gap-2 items-center">
+        <User size={20} />
+        View {artists.length} artists
+      </a>
+
+      <a
+        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent max-w-sm cursor-pointer duration-300 transform transition hover:scale-103 ease-in-out flex gap-2 items-center"
+        href="/krater/random"
+      >
+        <Dices size={20} />
+        Get a random suggestion
+      </a>
     </div>
   )
 }
