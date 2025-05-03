dfeccbbfa849c4c084f25d9b2e17589d6972e465
balazs.saros <balazs.saros@gmail.com>
2025-05-03 15:27:29 +0200

    remove focus ring from the action buttons

--- src/components/action-button.tsx
+++ src/components/action-button.tsx
@@ -18,7 +18,7 @@ export const ActionButton: FC<ActionButtonProps> = ({
   size = 32,
 }) => {
   const className =
-    'p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent cursor-pointer duration-100 transform transition hover:bg-(--item) ease-in-out flex gap-2 items-center min-h-12 bg-(--card) disabled:cursor-not-allowed disabled:opacity-25 disabled:bg-transparent'
+    'p-2 rounded-lg cursor-pointer duration-100 transform transition hover:bg-(--item) ease-in-out flex gap-2 items-center min-h-12 bg-(--card) disabled:cursor-not-allowed disabled:opacity-25 disabled:bg-transparent'
   if (href) {
     return (
       <Link className={className} href={href}>
