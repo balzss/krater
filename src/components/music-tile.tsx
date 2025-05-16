44cfbd761d0c8913dfa4403c168f96ac255cc1b6
balazs.saros <balazs.saros@gmail.com>
2025-05-17 01:25:11 +0200

    add info button placeholder to release tiles

--- src/components/music-tile.tsx
+++ src/components/music-tile.tsx
@@ -1,7 +1,9 @@
-import { Fragment } from 'react'
+import { useState, Fragment } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import type { Artist } from '@/lib/data'
+import { BadgeInfo } from 'lucide-react'
+import { ActionButton } from '@/components'
 
 interface MusicTileProps {
   cover: string
@@ -22,9 +24,13 @@ const MusicTile: React.FC<MusicTileProps> = ({
   priority = false,
   className = '',
 }) => {
+  const [showInfoBtn, setShowInfoBtn] = useState<boolean>(false)
+  const isTouchDevice = window.matchMedia('(pointer: coarse)').matches
   return (
     <div
-      className={`bg-white shadow-sm rounded-lg overflow-hidden hover:shadow-md max-w-sm w-full transition duration-300 ${className}`}
+      className={`bg-white shadow-sm rounded-lg overflow-hidden hover:shadow-md max-w-sm w-full transition duration-300 flex flex-col ${className}`}
+      onMouseEnter={() => setShowInfoBtn(true)}
+      onMouseLeave={() => setShowInfoBtn(false)}
     >
       <Link
         className="flex relative w-full aspect-square cursor-pointer"
@@ -40,7 +46,7 @@ const MusicTile: React.FC<MusicTileProps> = ({
           priority={priority}
         />
       </Link>
-      <div className="p-4">
+      <div className="p-4 flex flex-col flex-grow">
         <Link
           className="text-lg font-bold text-gray-900 cursor-pointer hover:underline"
           href={media}
@@ -48,20 +54,28 @@ const MusicTile: React.FC<MusicTileProps> = ({
         >
           {title}
         </Link>
-        <p className="text-sm text-gray-600">
-          {artists.map((artist, index) => (
-            <Fragment key={artist?.rymId || index}>
-              {artist ? (
-                <Link className="hover:text-gray-500" href={`/browse?artist=${artist?.rymId}`}>
-                  {artist?.displayName}
-                </Link>
-              ) : (
-                <span>Unknown</span>
-              )}
-              {index < artists.length - 1 ? ', ' : ''}
-            </Fragment>
-          ))}
-        </p>
+        <div className="flex h-full">
+          <p className="text-sm text-gray-600 mr-auto">
+            {artists.map((artist, index) => (
+              <Fragment key={artist?.rymId || index}>
+                {artist ? (
+                  <Link className="hover:text-gray-500" href={`/browse?artist=${artist?.rymId}`}>
+                    {artist?.displayName}
+                  </Link>
+                ) : (
+                  <span>Unknown</span>
+                )}
+                {index < artists.length - 1 ? ', ' : ''}
+              </Fragment>
+            ))}
+          </p>
+          <ActionButton
+            onClick={() => alert('Coming soon...')}
+            icon={BadgeInfo}
+            className={`text-gray-600 -mr-2 -mb-2 self-end ${showInfoBtn || isTouchDevice ? 'opacity-100' : 'opacity-0'} transition duration-150`}
+            size={20}
+          />
+        </div>
       </div>
     </div>
   )
